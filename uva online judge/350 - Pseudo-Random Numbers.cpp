c6847323b3aae3a3c8397a11b5cc0cfac58811bb
Sandman17 <matheus.botelhoes@gmail.com>
2017-12-01 17:23:47 -0200

    Update 350 - Pseudo-Random Numbers.cpp

--- uva online judge/350 - Pseudo-Random Numbers.cpp
+++ uva online judge/350 - Pseudo-Random Numbers.cpp
@@ -7,24 +7,24 @@ int main()
 {
     vector <int> v;
     vector <int> ::iterator it;
-    int Z,I,M,L,i,ct=1;
-    while(1){
-        cin>>Z>>I>>M>>L;
-        if(Z==0&&I==0&&M==0&&L==0)break;
+    int Z,I,M,L,i,ct=1,d;
 
-        while(1){
+    do{
+        cin>>Z>>I>>M>>L;
+        d=1;
+        while(d){
         v.push_back(L);
         L = (Z*L+I)%M;
         it = find(v.begin(),v.end(),L);
         if(it!=v.end()){
             for(i=0;v[i]!=*it;i++){}
             cout<<"Case "<<ct<<": "<<v.size()-i<<endl;
-            break;
+            d--;
             }
         }
         v.clear();
         ct++;
 
-    }
+    }while(Z!=0&I!=0&M!=0&L!=0);
     return 0;
 }
